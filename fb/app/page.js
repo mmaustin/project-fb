eb49ff3b438880e61883706f56983512d5458188
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-01-03 12:17:04 -0500

    removed boilerplate css from globals css file

--- fb/app/page.js
+++ fb/app/page.js
@@ -1,18 +1,21 @@
-import { connectToDB } from '@/utils/database';
+//import { connectToDB } from '@/utils/database';
 //import Image from 'next/image';
 
 
 const HomePage = async () => {
 
-  try {
-    let db = await connectToDB();
-    console.log(db);
-  } catch (error) {
-    console.log(error);
-  }
+  // try {
+  //   let db = await connectToDB();
+  //   console.log(db);
+  // } catch (error) {
+  //   console.log(error);
+  // }
 
   return (
-    <div >Home Page</div>
+    <div >
+      <h1 className="text-3xl">Home Page</h1>
+    </div>
+
   )
 }
-export default HomePage
+export default HomePage;
