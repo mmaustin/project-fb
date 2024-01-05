cbd73a7e8572654e6cea43b32e9ed350787c773c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-01-05 08:28:45 -0500

    deleted all references to anything related to first mongo database and models

--- fb/utils/actions.js
+++
@@ -1,6 +0,0 @@
-import Baby from "@/models/baby";
-
-
-export const getBabies = async () => {
-  return await Baby.find({});
-};
--- fb/utils/database.js
+++ fb/utils/database.js
@@ -13,9 +13,7 @@ export const connectToDB = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "baby-names",
-    })
+    await mongoose.connect(process.env.MONGODB_URI);
     isConnected = true;
     console.log('MongoDB connected');
   } catch (error) {
