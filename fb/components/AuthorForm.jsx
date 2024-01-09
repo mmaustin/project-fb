30148b1b3c222a8474fe822f16d34f75e26aca82
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-01-09 10:09:21 -0500

    set up author form component

---
+++ fb/components/AuthorForm.jsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { createAuthor } from "@/utils/actions";
+//import { useEffect } from "react";
+import { useFormStatus, useFormState } from 'react-dom';
+
+const SubmitBtn = () => {
+  const { pending } = useFormStatus();
+
+  return (
+    <button type="submit" className="btn btn-primary join-item" disabled={pending}>
+      {pending ? 'please wait . . .' : 'create task'}
+    </button>
+  )
+};
+
+const initialState = { message: null };
+
+const AuthorForm = () => {
+
+  const [state, formAction] = useFormState(createAuthor, initialState);
+
+  return (
+    <form action={formAction}>
+      {state.message ? <p className="mb-2">{state.message}</p> : null}
+      <div className="join w-full">
+        <input type="text" className="input input-bordered join-item w-full" placeholder="type here" name="name" required />
+        <input type="text" className="input input-bordered join-item w-full" placeholder="type here" name="style" required />
+        <input type="text" className="input input-bordered join-item w-full" placeholder="type here" name="authorInfluence" required />
+        <input type="text" className="input input-bordered join-item w-full" placeholder="type here" name="workInfluence" required />
+        <SubmitBtn />
+      </div>
+    </form>
+  )
+}
+export default AuthorForm;
