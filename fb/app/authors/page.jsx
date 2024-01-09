71b219ed828cca9587e56d9993878d13b163d193
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-01-09 10:13:58 -0500

    created author form page

---
+++ fb/app/authors/page.jsx
@@ -0,0 +1,10 @@
+import AuthorForm from "@/components/AuthorForm"
+
+const ShowAuthors = () => {
+  return (
+    <div className="max-w-lg">
+      <AuthorForm />
+    </div>
+  )
+}
+export default ShowAuthors
