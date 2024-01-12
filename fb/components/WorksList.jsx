4533e91c8a286cf6c5ee753e0aa87fd093b397ee
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-01-12 10:29:46 -0500

    completed works list with proper works information

--- fb/components/WorksList.jsx
+++ fb/components/WorksList.jsx
@@ -1,37 +1,39 @@
 import { getAuthorsWorks } from "@/utils/actions";
+//import Link from "next/link";
+
 
 
 const WorksList = async () => {
-  const allAuthors = await getAuthors();
+  const allAuthorsWorks = await getAuthorsWorks();
 
-  if (allAuthors.length === 0) {
+  if (allAuthorsWorks.length === 0) {
     return (
-      <h2 className="mt-8 font-medium text-lg">No authors are writing . . . </h2>
+      <h2 className="mt-8 font-medium text-lg">Author has created no works . . . </h2>
     )
   }
 
   return (
     <div className="mt-8">
-      {allAuthors.map(author => {
-        return <div key={author._id} className="flex-col justify-between items-center px-6 py-4 mb-4 border border-base-300 rounded-lf shadow-lg">
+      {allAuthorsWorks.map(work => {
+        return <div key={work._id} className="flex-col justify-between items-center px-6 py-4 mb-4 border border-base-300 rounded-lf shadow-lg">
           <h4 className="text-lg capitalize">
-            {author.name}
+            {work.title}
           </h4>
           <h4 className="text-lg capitalize">
-            {author.style}
+            {author.genre}
           </h4>
           <h4 className="text-lg capitalize">
-            {author.authorInfluence}
+            {work.synopsis}
           </h4>
           <h4 className="text-lg capitalize">
-            {author.workInfluence}
+            {work.wordCount}
           </h4>
-          <Link href={`/authors/${author._id}`} className="btn btn-accent btn-xs" >
+          {/* <Link href={`/authors/${author._id}`} className="btn btn-accent btn-xs" >
             Your Page
-          </Link>
+          </Link> */}
         </div>
       })}
     </div>
   )
 }
-export default WorksList
+export default WorksList;
