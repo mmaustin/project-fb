f27bf3a36b67a4e5252f75ccbd7373b0d688f9fc
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-01-17 08:43:14 -0500

    wrapped app code in clerkprovider

--- fb/app/layout.js
+++ fb/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/Navbar';
+import { ClerkProvider } from '@clerk/nextjs'
 
 
 const inter = Inter({ subsets: ['latin'] });
@@ -12,13 +13,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Navbar />
-        <main className="px-8 py-20 max-w-6xl mx-auto">
-          {children}
-        </main>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Navbar />
+          <main className="px-8 py-20 max-w-6xl mx-auto">
+            {children}
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 };
