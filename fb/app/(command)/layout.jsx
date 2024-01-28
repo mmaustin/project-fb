edd3f2109c216eec608b5a09a5cde183f1c9a9ff
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-01-28 10:03:50 -0500

    created sidebar component and imported to the command layout

--- fb/app/(command)/layout.jsx
+++ fb/app/(command)/layout.jsx
@@ -1,4 +1,5 @@
-
+import Sidebar from "@/components/Sidebar";
+import { FaBarsStaggered } from 'react-icons/fa6'
 
 const layout = ({ children }) => {
   return <div>{children}</div>
---
+++ fb/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+
+
+
+const Sidebar = () => {
+  return (
+    <div className="text-6xl">Sidebar</div>
+  )
+}
+export default Sidebar;
