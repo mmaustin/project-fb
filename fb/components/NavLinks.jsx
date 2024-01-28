f2179d031739bc9f52d8cc300bd06c90ebc171a2
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-01-28 11:15:44 -0500

    set up page links in profile and moved additional pages inside the command

--- fb/components/NavLinks.jsx
+++ fb/components/NavLinks.jsx
@@ -1,8 +1,23 @@
+import Link from "next/link";
 
+
+const links = [
+  { href: '/authors', label: 'authors' },
+  { href: '/works', label: 'works' },
+  { href: '/encouragements', label: 'encouragements' }
+];
 
 const NavLinks = () => {
-  return (
-    <div>NavLinks</div>
-  )
+  return <div className="menu text-base-content">
+    {links.map(link => {
+      return (
+        <li key={link.href}>
+          <Link href={link.href} className="capitalize">
+            {link.label}
+          </Link>
+        </li>
+      )
+    })}
+  </div>
 }
 export default NavLinks;
--- fb/components/SidebarHeader.jsx
+++ fb/components/SidebarHeader.jsx
@@ -1,8 +1,13 @@
-
+import ThemeToggle from "./ThemeToggle";
+import { FaLightbulb } from "react-icons/fa6";
 
 const SidebarHeader = () => {
   return (
-    <div>SidebarHeader</div>
+    <div className="flex items-center mb-4 gap-4 px-4">
+      <FaLightbulb className="w-10 h-10 text-primary" />
+      <h2 className="text-xl font-extrabold text-primary">Novel Ideas</h2>
+      <ThemeToggle />
+    </div>
   )
 }
 export default SidebarHeader;
