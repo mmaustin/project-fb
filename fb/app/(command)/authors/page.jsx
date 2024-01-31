f23ba3dec27995815c77e84fd583dec2e76b4dba
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-01-31 13:23:04 -0500

    added daisyui themes to tailwind config file

--- fb/app/(command)/authors/page.jsx
+++ fb/app/(command)/authors/page.jsx
@@ -6,6 +6,7 @@ import { auth, currentUser } from "@clerk/nextjs";
 const ShowAuthors = async () => {
   const { userId } = auth();
   const user = await currentUser();
+  //Both values remain the same after repeated logout/logins, good for createdBy model attributes
   console.log(userId, user.primaryEmailAddressId);
   return (
     <div className="max-w-lg">
