c6d7661a926689559adfe5850c3a065f55700631
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-01-31 13:29:39 -0500

    turned themetoggle comp into a client component at set initial theme state

--- fb/components/ThemeToggle.jsx
+++ fb/components/ThemeToggle.jsx
@@ -1,7 +1,17 @@
+"use client"
 
+import { useState } from "react";
+import { BsMoonFill, BsSunFill } from 'react-icons/bs';
 
+const themes = {
+  winter: 'winter',
+  dracula: 'dracula'
+}
 
 const ThemeToggle = () => {
+
+  const [theme, setTheme] = useState(themes.winter);
+
   return (
     <button className="btn btn-primary btn-sm">Toggle</button>
   )
