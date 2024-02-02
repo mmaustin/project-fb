7f11e1974903c9bbc26f0a8b1712cfab47df1c54
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-02-01 19:05:13 -0500

    imported react query and set up in provider component

--- fb/app/providers.jsx
+++ fb/app/providers.jsx
@@ -1,13 +1,26 @@
 'use client'
 
+
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 
 const Providers = ({ children }) => {
+  const [queryClient] = useState(() => {
+    return new QueryClient({
+      defaultOptions: {
+        queries: {
+          staleTime: 60 * 1000,
+        }
+      }
+    })
+  })
   return (
-    <>
+    <QueryClientProvider>
       <Toaster position="top-center" />
       {children}
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
-export default Providers
+export default Providers;
