1cc5784729bccc86f178787c15bcfa3643c1c68f
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-02-03 13:02:17 -0500

    used object properties to grab entries from formdata

--- fb/components/AuthorForm.jsx
+++ fb/components/AuthorForm.jsx
@@ -5,15 +5,15 @@ import { createAuthor } from "@/utils/actions";
 //import { useEffect } from "react";
 //import { useFormStatus, useFormState } from 'react-dom';
 
-const SubmitBtn = () => {
-  const { pending } = useFormStatus();
+// const SubmitBtn = () => {
+//   const { pending } = useFormStatus();
 
-  return (
-    <button type="submit" className="btn btn-primary join-item" disabled={pending}>
-      {pending ? 'please wait . . .' : 'Author'}
-    </button>
-  )
-};
+//   return (
+//     <button type="submit" className="btn btn-primary join-item" disabled={pending}>
+//       {pending ? 'please wait . . .' : 'Author'}
+//     </button>
+//   )
+// };
 //[action.payload.name]: action.payload.value,
 //const initialState = { message: null };
 
@@ -22,22 +22,25 @@ const AuthorForm = () => {
   // const ref = useRef(null);
   // const [state, formAction] = useFormState(createAuthor, initialState);
 
-  const handleSubmit = async () => {
-    const formData = new FormData();
-    console.log(formData);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const author = Object.fromEntries(formData.entries());
+    console.log(author);
   }
 
   return (
-    <form action={handleSubmit
-      //ref?.current.reset();
-    }>
-      {state.message !== 'a' ? <p className="mb-2">{state.message}</p> : null}
+    <form onSubmit={handleSubmit}
+    //action={handleSubmit
+    //ref?.current.reset();
+    >
+      {/* {state.message !== 'a' ? <p className="mb-2">{state.message}</p> : null} */}
       <div className=" w-full">
         <input type="text" className="input input-bordered join-item w-full" placeholder="Name" name="name" required />
         <input type="text" className="input input-bordered join-item w-full" placeholder="Style" name="style" required />
         <input type="text" className="input input-bordered join-item w-full" placeholder="Influenced By" name="authorInfluence" required />
         <input type="text" className="input input-bordered join-item w-full" placeholder="A Favorite Work" name="workInfluence" required />
-        <SubmitBtn />
+        <button className="btn btn-primary join-item" type="submit">Author</button>
       </div>
     </form>
   )
