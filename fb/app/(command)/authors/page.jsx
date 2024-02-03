1d17141727b57917cb4706ca0c3cd81b12e288cf
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-02-03 12:27:12 -0500

    wrapped author form component in query hydration in show authors component

--- fb/app/(command)/authors/page.jsx
+++ fb/app/(command)/authors/page.jsx
@@ -1,18 +1,26 @@
 import AuthorForm from "@/components/AuthorForm";
 import AuthorList from "@/components/AuthorList";
 import { auth, currentUser } from "@clerk/nextjs";
-
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 
 const ShowAuthors = async () => {
+  const queryClient = new QueryClient();
+
   const { userId } = auth();
-  const user = await currentUser();
+  //const user = await currentUser();
   //Both values remain the same after repeated logout/logins, good for createdBy model attributes
-  console.log(userId, user.primaryEmailAddressId);
+  //console.log(userId, user.primaryEmailAddressId);
   return (
-    <div className="max-w-lg">
-      <AuthorForm />
+    <>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <AuthorForm />
+      </HydrationBoundary>
       <AuthorList />
-    </div>
+    </>
+    // <div className="max-w-lg">
+    //   <AuthorForm />
+    //   <AuthorList />
+    // </div>
   )
 }
 export default ShowAuthors;
