11cc8dbaea1ea42e0c22104e96e78d244663593e
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-02-12 16:19:57 -0500

    wrapped workform component in react-query hydration boundary

--- fb/app/(command)/authors/[id]/page.jsx
+++ fb/app/(command)/authors/[id]/page.jsx
@@ -1,15 +1,20 @@
 import WorkForm from "@/components/WorkForm";
 import WorksList from "@/components/WorksList";
 import Link from "next/link";
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 
 
 const AuthorPage = ({ params }) => {
+  const queryClient = new QueryClient();
+
   const authorParamId = params.id;
   return (
     <div>
       <p>Author's Page</p>
-      <WorkForm authorId={authorParamId} />
-      {/* <WorksList authorId={authorParamId} /> */}
+      <HydrationBoundary state={dehydrate(queryClient)} >
+        <WorkForm authorId={authorParamId} />
+      </HydrationBoundary>
+      <WorksList authorId={authorParamId} />
     </div>
   )
 }
