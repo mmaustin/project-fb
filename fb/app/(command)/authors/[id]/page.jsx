7252c1542c1c8e1762e678e0140abce748ad5b40
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-02-12 21:38:20 -0500

    converted author model object's id into a string in order to make it passable as a prop

--- fb/app/(command)/authors/[id]/page.jsx
+++ fb/app/(command)/authors/[id]/page.jsx
@@ -2,17 +2,22 @@ import WorkForm from "@/components/WorkForm";
 import WorksList from "@/components/WorksList";
 import Link from "next/link";
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import { getSingleAuthor } from "@/utils/actions";
 
 
-const AuthorPage = ({ params }) => {
+const AuthorPage = async ({ params }) => {
   const queryClient = new QueryClient();
+  const author = await getSingleAuthor(params.id);
+
+  const retrievedAuthor = { aId: author._id.toString(), aAuthorName: author.authorName, aAboutMe: author.aboutMe, aAuthorInfluence: author.authorInfluence, aWorkInfluence: author.workInfluence, aAuthUser: author.authUser, aPublicProfile: author.publicProfile }
+
 
   const authorParamId = params.id;
   return (
     <div>
       <p>Author's Page</p>
       <HydrationBoundary state={dehydrate(queryClient)} >
-        <WorkForm authorId={authorParamId} />
+        <WorkForm workAuthor={retrievedAuthor} />
       </HydrationBoundary>
       <WorksList authorId={authorParamId} />
     </div>
--- fb/components/WorkForm.jsx
+++ fb/components/WorkForm.jsx
@@ -1,46 +1,29 @@
 "use client"
 
-import { createWork } from "@/utils/actions";
-import { useRef, useState } from "react";
-import { useFormStatus, useFormState } from 'react-dom';
+import { createWork, getSingleAuthor } from "@/utils/actions";
+import { useState } from "react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 
-const initialState = { number: 1 };
-
-
-const WorkForm = ({ authorId }) => {
-  const ref = useRef(null);
-  const sendWithAuthorId = createWork.bind(null, authorId);
-
-
-  const [state, formAction] = useFormState(sendWithAuthorId, initialState);
-
-  const [stat, setStat] = useState(1);
-
+const WorkForm = ({ workAuthor }) => {
+  // const workAuthor = await getSingleAuthor(authorId);
+  console.log(workAuthor);
 
   return (
-    <form ref={ref} action={formAction
-      //function () {
-      //ref?.current.reset();
-      //formAction;
-      //setStat(stat + 1);
-      //await createWork(authorId, formData);
-    }>
-      {/* {state.message !== 'a' ? <p className="mb-2">{state.message}</p> : null} */}
+    <div>
+      work it out!
+    </div>
+    // <form>
 
-      {state.number > stat && <p>success</p>}
-      {state.number < stat && <p>error</p>}
-
-      <div className=" w-full">
-        <input type="text" className="input input-bordered join-item w-full" placeholder="title" name="title" required />
-        <input type="text" className="input input-bordered join-item w-full" placeholder="genre" name="genre" required />
-        <input type="text" className="input input-bordered join-item w-full" placeholder="synopsis" name="synopsis" required />
-        <input type="number" className="input input-bordered join-item w-full" placeholder="word count" name="wordCount" />
-        <button type="submit">Create Work</button>
-      </div>
-    </form>
+    //   <div className=" w-full">
+    //     <input type="text" className="input input-bordered join-item w-full" placeholder="title" name="title" required />
+    //     <input type="text" className="input input-bordered join-item w-full" placeholder="genre" name="genre" required />
+    //     <input type="text" className="input input-bordered join-item w-full" placeholder="synopsis" name="synopsis" required />
+    //     <input type="number" className="input input-bordered join-item w-full" placeholder="word count" name="wordCount" />
+    //     <button type="submit">Create Work</button>
+    //   </div>
+    // </form>
   )
 }
 export default WorkForm;
