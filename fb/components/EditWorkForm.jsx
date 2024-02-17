ae6371ba16c030ff722169b9e71745959e53c206
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-02-17 12:27:32 -0500

    created editwork state object and handleinputchange function in edit work form component

--- fb/components/EditWorkForm.jsx
+++ fb/components/EditWorkForm.jsx
@@ -1,9 +1,38 @@
 "use client"
 
+import { editWork } from "@/utils/actions";
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
 
 const EditWorkForm = ({ workToEdit }) => {
 
   const { title, genre, synopsis, authorName, authUser, writingStage, createdBy } = workToEdit;
+
+  const [editWorkData, setEditWorkData] = useState({
+    title,
+    genre,
+    synopsis,
+    authorName,
+    authUser,
+    writingStage,
+    createdBy
+  });
+
+  const writingStateOptions = ['Brainstorming', 'Drafting', 'Editing'].map((opt, i) => {
+    return (
+      <option key={`${opt}: ${i}`} value={opt}>{opt}</option>
+    )
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setEditWorkData((prevEditWorkData) => ({
+      ...prevEditWorkData,
+      [name]: value,
+    }));
+  };
 
   return (
     <div>{workToEdit.title}</div>
