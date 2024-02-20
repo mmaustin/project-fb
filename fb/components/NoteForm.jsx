f62ea05ce1889b54428d84fd49375bf3bed4d43f
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-02-20 07:14:54 -0500

    created notesform component to handle creation of notes

---
+++ fb/components/NoteForm.jsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { createNote } from "@/utils/actions"
+import { useMutation } from "@tanstack/react-query"
+import { useState } from "react"
+import toast from "react-hot-toast"
+
+
+const NoteForm = () => {
+  return (
+    <div>Note Form</div>
+  )
+}
+export default NoteForm
