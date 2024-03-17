a2c758e78137298bb3963da956097101f7db8ff0
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-03-17 15:48:07 -0400

    imported themetoggle component to the homepage

--- fb/app/page.js
+++ fb/app/page.js
@@ -1,4 +1,4 @@
-
+import ThemeToggle from "@/components/ThemeToggle";
 import Link from "next/link";
 
 
@@ -10,17 +10,22 @@ const HomePage = () => {
   //   // console.log(data);
 
   return (
-    <div className="hero min-h-screen ">
-      <div className="hero-content text-center">
-        <div className="max-w-3xl">
-          <h1 className="text-6xl font-bold uppercase border-t-2 border-dotted">
-            Novel Ideas
-            <p className="py-6 text-xl font-normal leading-loose normal-case border-t-2 border-dotted">Stop relying on sticky notes, napkins, and your "perfect" memory. Store all of your notes here.  Your book will thank you.</p>
-          </h1>
-          <Link href="/authors" className="btn btn-secondary rounded-lg ">Get Started</Link>
+    <>
+      <div className="m-2">
+        <ThemeToggle />
+      </div>
+      <div className="hero min-h-screen ">
+        <div className="hero-content text-center">
+          <div className="max-w-3xl">
+            <h1 className="text-6xl font-bold uppercase border-t-2 border-dotted">
+              Novel Ideas
+              <p className="py-6 text-xl font-normal leading-loose normal-case border-t-2 border-dotted">Stop relying on sticky notes, napkins, and your "perfect" memory. Store all of your notes here.  Your book will thank you.</p>
+            </h1>
+            <Link href="/authors" className="btn btn-secondary rounded-lg ">Get Started</Link>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 export default HomePage;
