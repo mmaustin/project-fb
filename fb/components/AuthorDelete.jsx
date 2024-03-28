757404218db8b51e07daa7096ceb9dea67b6342b
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-03-27 21:51:35 -0400

    used tooltip to warn user before delete their author object

--- fb/components/AuthorDelete.jsx
+++ fb/components/AuthorDelete.jsx
@@ -9,7 +9,7 @@ const AuthorDelete = ({ authorId }) => {
   }
 
   return (
-    <button onClick={deleteHandler} className="btn btn-info btn-xs rounded-lg" type="button">Delete Author</button>
+    <button onClick={deleteHandler} className="btn btn-info btn-xs rounded-lg tooltip tooltip-top tooltip-error uppercase" data-tip="You Sure? You Will Lose Your Author Object, All Works, & all Notes." type="button">Delete Author</button>
   )
 };
 export default AuthorDelete;
