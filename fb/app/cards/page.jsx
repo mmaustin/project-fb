4f680da0801d084578d0eeb1f67cae8945236565
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-04-25 10:21:30 -0400

    commented out cards app page

--- fb/app/cards/page.jsx
+++ fb/app/cards/page.jsx
@@ -1,28 +1,28 @@
-import { getContestants } from "@/utils/actions";
-import { connectToDB } from "@/utils/database";
+// import { getContestants } from "@/utils/actions";
+// import { connectToDB } from "@/utils/database";
 
 
-const ShowCards = async () => {
+// const ShowCards = async () => {
 
-  //connectToDB is definitely needed when using an http handler
-  await connectToDB();
-  const contestants = await getContestants();
+//connectToDB is definitely needed when using an http handler
+// await connectToDB();
+// const contestants = await getContestants();
 
-  // const response = await fetch('http://localhost:3000/api/contestants');
-  // const data = await response.json();
-  // console.log(data);
+// const response = await fetch('http://localhost:3000/api/contestants');
+// const data = await response.json();
+// console.log(data);
 
-  return (
-    <div>
-      <p>Contestants Page</p>
-      {contestants.map(c => {
-        return (
-          <p key={c._id}>
-            {c.name}
-          </p>
-        )
-      })}
-    </div>
-  )
-}
-export default ShowCards
+//   return (
+//     <div>
+//       <p>Contestants Page</p>
+//       {contestants.map(c => {
+//         return (
+//           <p key={c._id}>
+//             {c.name}
+//           </p>
+//         )
+//       })}
+//     </div>
+//   )
+// }
+// export default ShowCards
