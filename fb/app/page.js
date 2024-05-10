0ae427d071c28b711d5affcb8a54dca9bf0fd8e2
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-05-10 17:23:00 -0400

    removed daisy ui hero component from homepage

--- fb/app/page.js
+++ fb/app/page.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const HomePage = () => {
   const { userId } = auth();
-  console.log(userId);
+  // console.log(userId);
   //   // cache no-store is necessary. otherwise, you'll get the very first fetch every time--i think.
 
   //   // const response = await fetch('http://localhost:3000/api/contestants', { cache: 'no-store' });
@@ -12,21 +12,24 @@ const HomePage = () => {
   //   // console.log(data);
 
   return (
-    <div className="flex flex-col justify-center items-start">
-      <div className="m-2 w-40 w align-middle">
+    <div className="flex flex-col justify-center items-center">
+      <div className="m-2 w-40 flex items-center">
         <ThemeToggle />
       </div>
-      <div className="hero min-h-screen ">
-        <div className="hero-content text-center">
-          <div className="max-w-3xl">
-            <h1 className="text-6xl font-bold uppercase border-t-2 border-dotted">
-              Novel Ideas
-              <p className="py-6 text-xl font-normal leading-loose normal-case border-t-2 border-dotted">Stop relying on sticky notes, napkins, and your "perfect" memory. Store all of your notes here.  Your book will thank you.</p>
-            </h1>
-            <Link href="/authors" className="btn btn-secondary rounded-lg ">Get Started</Link>
-          </div>
+      {/* <div className="hero min-h-screen ">
+        <div className="hero-content text-center"> */}
+      <div className="flex justify-center items-center">
+        {/* max-w-3xl */}
+        <div className="max-w-3xl flex flex-col justify-center items-center">
+          <h1 className="text-6xl font-bold uppercase border-t-2 border-dotted text-center">
+            Novel Ideas
+            <p className="py-6 text-xl font-normal leading-loose normal-case border-t-2 border-dotted">Stop relying on sticky notes, napkins, and your "perfect" memory. Store all of your notes here.  Your book will thank you.</p>
+          </h1>
+          <Link href="/authors" className="btn btn-secondary flex w-48 items-center rounded-lg ">Get Started</Link>
         </div>
       </div>
+      {/* </div>
+      </div> */}
     </div>
   )
 }
