6d683330f3b236b60a74dcdad0c1b72649c50366
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-05-10 18:06:32 -0400

    added link to shell about page to homepage

--- fb/app/page.js
+++ fb/app/page.js
@@ -12,20 +12,23 @@ const HomePage = () => {
   //   // console.log(data);
 
   return (
-    <div className="flex flex-col justify-center items-center">
-      <div className="m-2 w-40 flex items-center">
-        <ThemeToggle />
+    <div className="mx-5 flex flex-col justify-center items-center">
+      <div className="my-10 w-full flex justify-center items-center">
+        <div className="w-full flex justify-between items-center">
+          <ThemeToggle />
+          <Link href="/about" className="btn btn-secondary flex w-32 items-center rounded-lg ">About Page</Link>
+        </div>
       </div>
       {/* <div className="hero min-h-screen ">
         <div className="hero-content text-center"> */}
-      <div className="flex justify-center items-center">
+      <div className="mt-32 flex justify-center items-center">
         {/* max-w-3xl */}
         <div className="max-w-3xl flex flex-col justify-center items-center">
-          <h1 className="text-6xl font-bold uppercase border-t-2 border-dotted text-center">
+          <h1 className="text-3xl md:text-5xl font-bold uppercase border-t-2 border-dotted text-center">
             Novel Ideas
-            <p className="py-6 text-xl font-normal leading-loose normal-case border-t-2 border-dotted">Stop relying on sticky notes, napkins, and your "perfect" memory. Store all of your notes here.  Your book will thank you.</p>
+            <p className="py-6 text-sm md:text-xl font-normal leading-loose normal-case border-t-2 border-dotted">Stop relying on sticky notes, napkins, and your "perfect" memory. Store all of your notes here.  Your book will thank you.</p>
           </h1>
-          <Link href="/authors" className="btn btn-secondary flex w-48 items-center rounded-lg ">Get Started</Link>
+          <Link href="/authors" className="btn btn-secondary flex w-32 items-center rounded-lg ">Get Started</Link>
         </div>
       </div>
       {/* </div>
