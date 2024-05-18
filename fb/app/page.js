d6206bbde1f355d2e8aa1db5cf5054892b88946c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-05-18 13:36:19 -0400

    removed commented code from homepage, commented out about page link

--- fb/app/page.js
+++ fb/app/page.js
@@ -4,25 +4,16 @@ import Link from "next/link";
 
 const HomePage = () => {
   const { userId } = auth();
-  // console.log(userId);
-  //   // cache no-store is necessary. otherwise, you'll get the very first fetch every time--i think.
-
-  //   // const response = await fetch('http://localhost:3000/api/contestants', { cache: 'no-store' });
-  //   // const data = await response.json();
-  //   // console.log(data);
 
   return (
     <div className="mx-5 flex flex-col justify-center items-center">
       <div className="my-10 w-full flex justify-center items-center">
         <div className="w-full flex justify-between items-center">
           <ThemeToggle />
-          <Link href="/about" className="btn btn-secondary flex w-32 items-center rounded-lg ">About Page</Link>
+          {/* <Link href="/about" className="btn btn-secondary flex w-32 items-center rounded-lg ">About Page</Link> */}
         </div>
       </div>
-      {/* <div className="hero min-h-screen ">
-        <div className="hero-content text-center"> */}
       <div className="mt-32 flex justify-center items-center">
-        {/* max-w-3xl */}
         <div className="max-w-3xl flex flex-col justify-center items-center">
           <h1 className="text-3xl md:text-5xl font-bold uppercase border-t-2 border-dotted text-center">
             Novel Ideas
@@ -31,8 +22,6 @@ const HomePage = () => {
           <Link href="/authors" className="btn btn-secondary flex w-32 items-center rounded-lg ">Get Started</Link>
         </div>
       </div>
-      {/* </div>
-      </div> */}
     </div>
   )
 }
