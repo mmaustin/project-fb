0360a973e9bab19f826c74b60b03c5553175f215
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-06-02 14:13:18 -0400

    used custom actions to place all of an author's assets in an array

--- fb/app/(command)/activity/page.jsx
+++ fb/app/(command)/activity/page.jsx
@@ -1,12 +1,17 @@
+import { getAuthorNotes, getAuthorWithAuth, getAuthorsWorks } from "@/utils/actions";
 import { auth } from "@clerk/nextjs";
 
-const AuthorActivityPage = () => {
+const AuthorActivityPage = async () => {
 
   const { userId } = auth();
-  console.log(userId);
+  let author = await getAuthorWithAuth(userId);
+  let authorWorks = await getAuthorsWorks(author._id);
+  let authorNotes = await getAuthorNotes(author.authorName);
+  let allAuthorAssets = [...authorWorks, ...authorNotes];
+  console.log(allAuthorAssets);
 
   return (
     <div>Author Activity Page</div>
   )
-}
-export default AuthorActivityPage
+};
+export default AuthorActivityPage;
