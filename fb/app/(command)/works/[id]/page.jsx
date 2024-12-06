05bc7b7ee76a6ea1bf5c7dd8deeaf4d29388ec68
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-12-06 13:22:30 -0500

    Restyling of work page info card

--- fb/app/(command)/works/[id]/page.jsx
+++ fb/app/(command)/works/[id]/page.jsx
@@ -19,11 +19,38 @@ const SingleWorkPage = async ({ params }) => {
 
   const noteWorkProperties = { authUser: work.authUser, authorName: work.authorName, authorId: work.createdBy.toString(), workId: work._id.toString() };
 
+  // <div className="card bg-base-100 w-96 shadow-xl">
+  //   <figure>
+  //     <img
+  //       src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+  //       alt="Shoes" />
+  //   </figure>
+  //   <div className="card-body">
+  //     <h2 className="card-title">Shoes!</h2>
+  //     <p>If a dog chews shoes whose shoes does he choose?</p>
+  //     <div className="card-actions justify-end">
+  //       <button className="btn btn-primary">Buy Now</button>
+  //     </div>
+  //   </div>
+  // </div>
+
   return (
-    <div className="grid gap-4 md:grid-cols-2 items-start w-full">
+    <div className="grid gap-4 md:grid-cols-2 items-start w-full border border-red">
       {/* <div className="w-full flex flex-col justify-center items-center"> */}
-      <div key={work._id} className="w-80 md:w-96 mx-8 flex flex-col justify-center items-start px-6 py-4 mb-4 rounded-lg shadow-lg">
-        <h4 className="text-lg capitalize font-bold text-blue">
+      <div key={work._id} className="card bg-base-400 w-full shadow-xl border border-green">
+        <div className="">{work.synopsis}</div>
+        <div className="card-body">
+          <h2 className="card-title">
+            {work.title}
+          </h2>
+          <p>{work.writingStage}</p>
+          <div className="card-actions justify-center">
+            <Link href={`/works/edit/${work._id}`} className="btn btn-accent btn-xs border-x-base-100 rounded-lg" >
+              Edit Work
+            </Link>
+          </div>
+        </div>
+        {/* <h4 className="text-lg capitalize font-bold text-blue">
           {work.title}
         </h4>
         <h4 className="text-lg capitalize font-bold">
@@ -40,7 +67,7 @@ const SingleWorkPage = async ({ params }) => {
         </h4>
         <Link href={`/works/edit/${work._id}`} className="btn btn-accent btn-xs join-item rounded-lg" >
           Edit Work
-        </Link>
+        </Link> */}
       </div>
       <div className="w-80 mx-8 md:w-96 flex flex-col justify-center items-start px-6 py-4 mb-4 rounded-lg shadow-lg" >
         <HydrationBoundary state={dehydrate(queryClient)}>
