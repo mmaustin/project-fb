e241c61199ab1e215691a8471b108e39ffab6017
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-12-10 10:42:03 -0500

    Commented out getChartsAction until ready to fully implement

--- fb/app/(command)/stats/page.jsx
+++ fb/app/(command)/stats/page.jsx
@@ -1,11 +1,11 @@
 import AuthorChart from "@/components/visualization/AuthorChart";
-import { getChartStats } from "@/utils/actions";
+// import { getChartStats } from "@/utils/actions";
 import { auth } from "@clerk/nextjs";
 
 const AuthorStats = async () => {
 
-  const notesObj = await getChartStats();
-  console.log(notesObj);
+  // const notesObj = await getChartStats();
+  // console.log(notesObj);
 
 
   const { userId } = auth();
