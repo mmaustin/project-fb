01789587cbad2b18edbf4eb1d90683e9ab1902e6
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-12-11 14:29:33 -0500

    Attempted to implement recharts in a server component, errors

--- fb/app/(command)/stats/page.jsx
+++ fb/app/(command)/stats/page.jsx
@@ -1,6 +1,7 @@
 import AuthorChart from "@/components/visualization/AuthorChart";
 import { getChartStats } from "@/utils/actions";
 import { auth } from "@clerk/nextjs";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, } from 'recharts';
 
 const AuthorStats = async () => {
 
@@ -9,12 +10,26 @@ const AuthorStats = async () => {
   const notesObj = await getChartStats(userId);
   console.log(notesObj);
 
-
+  if (!notesObj || notesObj.length < 1) return null;
 
   return (
-    <div className="flex flex-wrap justify-center items-center w-96 h-auto shadow-2xl">
-      <AuthorChart authorId={userId} />
-    </div>
+    <section className='mt-16'>
+      <h1 className='text-4xl font-semibold text-center'>
+        Monthly Applications
+      </h1>
+      <ResponsiveContainer width='100%' height={300}>
+        <BarChart data={notesObj} margin={{ top: 50 }}>
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='date' />
+          <YAxis allowDecimals={false} />
+          {/* <Tooltip /> */}
+          <Bar dataKey='count' fill='#2563eb' barSize={75} />
+        </BarChart>
+      </ResponsiveContainer>
+    </section>
+    // <div className="flex flex-wrap justify-center items-center w-96 h-auto shadow-2xl">
+    //   <AuthorChart authorId={userId} />
+    // </div>
   )
 }
 export default AuthorStats;
