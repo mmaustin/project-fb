cbd2b8e745a8a51bf9d6441488e57c0b73c49e7b
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-12-14 12:31:15 -0500

    Pasted code from old stats page to new charts page to display authorNotesChart

--- fb/app/(command)/chart/page.jsx
+++ fb/app/(command)/chart/page.jsx
@@ -1,8 +1,14 @@
-
+import AuthorChart from "@/components/visualization/AuthorChart";
+import { auth } from "@clerk/nextjs";
 
 const AuthorNotesChart = () => {
+
+  const { userId } = auth();
+
   return (
-    <div>AuthorNotesChart</div>
-  )
-}
+    <div className="flex flex-wrap justify-center items-center w-96 h-auto shadow-2xl">
+      <AuthorChart authorId={userId} />
+    </div>
+  );
+};
 export default AuthorNotesChart;
