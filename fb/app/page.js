1f3fe2c11505df8e9b850631e7b50aa170236671
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-12-20 16:33:05 -0500

    Added new pic assets and resized with next image component

--- fb/app/page.js
+++ fb/app/page.js
@@ -16,10 +16,10 @@ const HomePage = () => {
       </div> */}
       <div className="mt-32 flex justify-center items-center">
         <div className="max-w-3xl flex flex-col justify-center items-center">
-          <h1 className="text-3xl md:text-5xl font-bold uppercase border-t-2 border-dotted text-center">
+          <div className="text-3xl md:text-5xl font-semibold uppercase text-center">
             Novel Ideas
-            <p className="py-6 text-sm md:text-xl font-normal leading-loose normal-case border-t-2 border-dotted">Stop relying on sticky notes, napkins, and your "perfect" memory. Store all of your notes here.  Your book will thank you.</p>
-          </h1>
+            <p className="py-6 text-sm md:text-xl font-serif leading-loose normal-case">Stop relying on sticky notes, napkins, and your "perfect" memory. Store all of your notes here.  Your book will thank you.</p>
+          </div>
           <div className="flex flex-wrap justify-center items-center">
             <Link href="/authors" className="btn btn-secondary flex w-32 items-center rounded-lg ">Get Started</Link>
             <div className="tooltip tooltip-bottom mt-10" data-tip="Outlook Is Not Recommended. May Experience Redirect Issues On Login.">
--- fb/components/ImageContainer.jsx
+++ fb/components/ImageContainer.jsx
@@ -1,62 +1,58 @@
 import Image from "next/image";
 // import appImg from "@/public/appImg.png"
+import profileForm from "@/public/profile-form.png"
+import workForm from "@/public/work-form.png"
+import noteForm from "@/public/note-form.png"
+import monthlyNotes from "@/public/monthly-notes.png"
+import categoryNotes from "@/public/category-notes.png"
 
 
 
 const ImageContainer = () => {
 
   return (
-    <p>pop off</p>
-    // <div className="my-6 grid gap-4 sm:grid-cols-2 lg:grid-cols-3 w-3/4 border">
-    //   <Image
-    //     className="h-[200px] w-[300px] object-contain border border-green"
-    //     src={workExample}
-    //     alt="create author profile form"
-    //     priority
-    //     height={265}
-    //     width={198}
-    //   />
-    //   <Image
-    //     className="h-[200px] w-[300px] object-contain border border-green"
-    //     src={appImg}
-    //     alt="create work form"
-    //     priority
-    //   // height={192}
-    //   // width={192}
-    //   />
-    //   <Image
-    //     className="h-[200px] w-[300px] object-contain border border-green"
-    //     src={appImg}
-    //     alt="create work form"
-    //     priority
-    //   // height={192}
-    //   // width={192}
-    //   />
-    //   <Image
-    //     className="h-[200px] w-[300px] object-contain border border-green"
-    //     src={noteForm}
-    //     alt="create note form"
-    //     priority
-    //   // height={192}
-    //   // width={192}
-    //   />
-    //   <Image
-    //     className="h-[200px] w-[300px] object-contain border border-green"
-    //     src={monthlyStats}
-    //     alt="your monthly activity"
-    //     priority
-    //   // height={192}
-    //   // width={192}
-    //   />
-    //   <Image
-    //     className="h-[200px] w-[300px] object-contain border border-green"
-    //     src={colorChart}
-    //     alt="charting your notes by category"
-    //     priority
-    //   // height={192}
-    //   // width={192}
-    //   />
-    // </div>
+    <div className="my-6 grid gap-4 sm:grid-cols-2 lg:grid-cols-3 w-3/4">
+      <Image
+        className="h-[200px] w-[300px] object-contain border border-base-300/50 hover:border-neutral-800 rounded-lg"
+        src={profileForm}
+        alt="create author profile form"
+        priority
+        height={265}
+        width={198}
+      />
+      <Image
+        className="h-[200px] w-[300px] object-contain border border-base-300/50 hover:border-neutral-800 rounded-lg"
+        src={workForm}
+        alt="create work form"
+        priority
+        height={265}
+        width={198}
+      />
+      <Image
+        className="h-[200px] w-[300px] object-contain border border-base-300/50 hover:border-neutral-800 rounded-lg"
+        src={noteForm}
+        alt="create a note"
+        priority
+        height={265}
+        width={198}
+      />
+      <Image
+        className="h-[200px] w-[300px] object-contain border border-base-300/50 hover:border-neutral-800 rounded-lg"
+        src={monthlyNotes}
+        alt="chart showing notes created by month"
+        priority
+        height={265}
+        width={198}
+      />
+      <Image
+        className="h-[200px] w-[300px] object-contain border border-base-300/50 hover:border-neutral-800 rounded-lg"
+        src={categoryNotes}
+        alt="graph showing notes by category"
+        priority
+        height={265}
+        width={198}
+      />
+    </div>
   )
 }
 export default ImageContainer
