ce319c58bba66f077737d7952a9e2830fbb65d4f
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-12-20 15:46:50 -0500

    Removed improperly sized image files

--- fb/components/ImageContainer.jsx
+++ fb/components/ImageContainer.jsx
@@ -1,39 +1,62 @@
 import Image from "next/image";
-//import pic from "@/public/gone-girl.jpg"
-//import myPic from "@/public/photo.jpeg"
-import appImg from "@/public/appImg.png"
-import profile from "@/public/profile.png"
+// import appImg from "@/public/appImg.png"
+
 
 
 const ImageContainer = () => {
 
   return (
-    <div className="my-6 grid gap-4 sm:grid-cols-2 lg:grid-cols-3 w-3/4 border">
-      <Image
-        className="h-[200px] w-[300px] object-contain border border-green"
-        src={profile}
-        alt="create author profile form"
-        priority
-      // height={192}
-      // width={192}
-      />
-      <Image
-        className="h-[200px] w-[300px] object-contain border border-green"
-        src={appImg}
-        alt="create work form"
-        priority
-      // height={192}
-      // width={192}
-      />
-      <Image
-        className="h-[200px] w-[300px] object-contain border border-green"
-        src={appImg}
-        alt="create work form"
-        priority
-      // height={192}
-      // width={192}
-      />
-    </div>
+    <p>pop off</p>
+    // <div className="my-6 grid gap-4 sm:grid-cols-2 lg:grid-cols-3 w-3/4 border">
+    //   <Image
+    //     className="h-[200px] w-[300px] object-contain border border-green"
+    //     src={workExample}
+    //     alt="create author profile form"
+    //     priority
+    //     height={265}
+    //     width={198}
+    //   />
+    //   <Image
+    //     className="h-[200px] w-[300px] object-contain border border-green"
+    //     src={appImg}
+    //     alt="create work form"
+    //     priority
+    //   // height={192}
+    //   // width={192}
+    //   />
+    //   <Image
+    //     className="h-[200px] w-[300px] object-contain border border-green"
+    //     src={appImg}
+    //     alt="create work form"
+    //     priority
+    //   // height={192}
+    //   // width={192}
+    //   />
+    //   <Image
+    //     className="h-[200px] w-[300px] object-contain border border-green"
+    //     src={noteForm}
+    //     alt="create note form"
+    //     priority
+    //   // height={192}
+    //   // width={192}
+    //   />
+    //   <Image
+    //     className="h-[200px] w-[300px] object-contain border border-green"
+    //     src={monthlyStats}
+    //     alt="your monthly activity"
+    //     priority
+    //   // height={192}
+    //   // width={192}
+    //   />
+    //   <Image
+    //     className="h-[200px] w-[300px] object-contain border border-green"
+    //     src={colorChart}
+    //     alt="charting your notes by category"
+    //     priority
+    //   // height={192}
+    //   // width={192}
+    //   />
+    // </div>
   )
 }
 export default ImageContainer
