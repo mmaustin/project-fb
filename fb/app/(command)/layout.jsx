b0341ba8450f5bf10d31cb9c8348eba7d6f890ac
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-01-03 14:15:16 -0500

    Changed height of the drawer side when screen width is less than small

--- fb/app/(command)/layout.jsx
+++ fb/app/(command)/layout.jsx
@@ -1,13 +1,12 @@
-'use client'
 import Sidebar from "@/components/Sidebar";
 import { FaBarsStaggered } from 'react-icons/fa6';
 
 const layout = ({ children }) => {
   return (
-    <div className="drawer lg:drawer-open">
+    <div className="drawer md:drawer-open">
       <input type="checkbox" id="my-drawer-2" className="drawer-toggle" />
       <div className="drawer-content">
-        <label htmlFor="my-drawer-2" className="drawer-button lg:hidden fixed top-6 right-6">
+        <label htmlFor="my-drawer-2" className="drawer-button md:hidden fixed top-6 right-6">
           <FaBarsStaggered className="w-6 h-6 text-primary" />
         </label>
         <div className="bg-base-100 flex flex-col justify-center items-center px-8 py-12 min-h-screen">{children}</div>
