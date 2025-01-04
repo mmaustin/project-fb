59838b5e085d3f4b828e7387d78f01d9593dd84b
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-01-04 13:53:34 -0500

    Applied tooltip on work and note delete components on sm screens and larger

--- fb/components/WorkDelete.jsx
+++ fb/components/WorkDelete.jsx
@@ -11,7 +11,7 @@ const WorkDelete = ({ workId }) => {
 
   return (
     <>
-      <button onClick={deleteHandler} className="btn btn-xs border-2 border-error rounded-lg tooltip tooltip-top tooltip-error" data-tip="You will delete this work." type="button">Delete<span className="ml-2 text-error sm:hidden">*</span></button>
+      <button onClick={deleteHandler} className="btn btn-xs border-2 border-error rounded-lg sm:tooltip tooltip-top tooltip-error" data-tip="You will delete this work." type="button">Delete<span className="ml-2 text-error sm:hidden">*</span></button>
     </>
   )
 }
