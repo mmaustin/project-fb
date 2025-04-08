3c01c16dd54511b06fbb6b04644d5426bd123820
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-04-08 15:26:44 -0400

    Added a new model, DemoWork

---
+++ fb/models/DemoWork.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const DemoWorkSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    genre: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    synopsis: {
+      type: String,
+      minLength: 5,
+      maxLength: 250,
+      default: 'Still Conceptualizing, Patience!',
+      trim: true,
+    },
+    authorName: {
+      type: String,
+      required: true
+    },
+    writingStage: {
+      type: String,
+      enum: ['Brainstorming', 'Drafting', 'Editing'],
+      default: 'Brainstorming'
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: 'DemoAuthor',
+      required: true,
+    }
+  },
+  { timestamps: true },
+);
+
+const DemoWork = mongoose.models.DemoWork || mongoose.model("DemoWork", DemoWorkSchema);
+export default DemoWork;
