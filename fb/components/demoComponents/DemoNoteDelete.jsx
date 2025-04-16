cc97bd26e607c275ebad509430a8e32d95695073
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-04-16 16:45:56 -0400

    Created demo note delete component

---
+++ fb/components/demoComponents/DemoNoteDelete.jsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { demoNoteDelete } from "@/utils/demoActions";
+
+
+const DemoNoteDelete = ({ noteId, workId }) => {
+
+  const deleteHandler = async () => {
+    await noteDelete(noteId, workId);
+  };
+
+  return (
+    <>
+      <button onClick={deleteHandler} className="btn btn-xs border-2 border-error rounded-lg sm:tooltip tooltip-top tooltip-error" data-tip="You will delete this note." type="button">Delete Note<span className="ml-2 text-error sm:hidden">*</span></button>
+    </>
+  )
+}
+export default DemoNoteDelete
