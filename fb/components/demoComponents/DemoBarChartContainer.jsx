1e06ef3d68241407463ecb964b6b6f4dc2a4aa97
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-04-18 14:49:41 -0400

    Created the bar chart to display monthly notes stats with recharts

--- fb/components/demoComponents/DemoBarChartContainer.jsx
+++ fb/components/demoComponents/DemoBarChartContainer.jsx
@@ -7,7 +7,20 @@ const DemoBarChartContainer = ({ allNotes }) => {
   if (!notesObj || notesObj.length < 1) return <p>There Are No Notes To Chart.</p>;
 
   return (
-    <div>DemoBarChartContainer</div>
+    <section className='mt-16'>
+      <h1 className='text-2xl font-serif text-center'>
+        Monthly Notes <span className='font-semibold text-sm'>(last two months)</span>
+      </h1>
+      <ResponsiveContainer width='100%' height={300}>
+        <BarChart data={allNotes} margin={{ top: 50 }}>
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='date' />
+          <YAxis allowDecimals={false} />
+          <Tooltip />
+          <Bar dataKey='count' fill='#2563eb' barSize={75} />
+        </BarChart>
+      </ResponsiveContainer>
+    </section>
   )
 }
 export default DemoBarChartContainer;
