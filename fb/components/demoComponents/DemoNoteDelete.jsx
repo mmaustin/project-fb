de6b5c24194e67e67a78e552d6318b2149376eab
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-04-20 12:34:47 -0400

    removed asterisk from small screen delete buttons

--- fb/components/demoComponents/DemoNoteDelete.jsx
+++ fb/components/demoComponents/DemoNoteDelete.jsx
@@ -11,7 +11,7 @@ const DemoNoteDelete = ({ noteId, workID }) => {
 
   return (
     <>
-      <button onClick={deleteHandler} className="btn btn-xs border-2 border-error rounded-lg sm:tooltip tooltip-top tooltip-error" data-tip="You will delete this note." type="button">Delete Note<span className="ml-2 text-error sm:hidden">*</span></button>
+      <button onClick={deleteHandler} className="btn btn-xs border-2 border-error rounded-lg sm:tooltip tooltip-top tooltip-error" data-tip="You will delete this note." type="button">Delete Note</button>
     </>
   )
 }
--- fb/components/demoComponents/DemoWorkDelete.jsx
+++ fb/components/demoComponents/DemoWorkDelete.jsx
@@ -12,7 +12,7 @@ const DemoWorkDelete = ({ workId }) => {
 
   return (
     <>
-      <button onClick={deleteHandler} className="btn btn-xs border-2 border-error rounded-lg sm:tooltip tooltip-top tooltip-error" data-tip="You will delete this work." type="button">Delete<span className="ml-2 text-error sm:hidden">*</span></button>
+      <button onClick={deleteHandler} className="btn btn-xs border-2 border-error rounded-lg sm:tooltip tooltip-top tooltip-error" data-tip="You will delete this work." type="button">Delete</button>
     </>
   )
 }
