633a2c693bd332d7cf275a1f61f06302851a2830
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-04-25 14:54:40 -0400

    Added character counts to all input fields on the author profile form

--- fb/components/AuthorForm.jsx
+++ fb/components/AuthorForm.jsx
@@ -57,16 +57,25 @@ const AuthorForm = ({ authUser }) => {
 
   return (
     <form onSubmit={handleSubmit} className="w-full flex flex-col justify-center items-center">
+
       <p className="capitalize font-bold text-sm sm:text-lg md:text-xl m-4">create your profile</p>
-      <div className="w-full sm:w-3/4 flex flex-col items-center border-2 border-base-300 bg-base-300 rounded-lg">
-        <input onChange={handleInputChange} type="text" className="input join-item w-full rounded-lg mb-1 capitalize" placeholder="Name (min: 1, max: 40)" name="authorName" value={authorData.authorName} required />
-        <input onChange={handleInputChange} type="text" className="input  join-item w-full rounded-lg mb-1 capitalize" placeholder="Influenced By (min: 1, max: 40)" name="authorInfluence" value={authorData.authorInfluence} required />
-        <input onChange={handleInputChange} type="text" className="input  join-item w-full rounded-lg mb-1 capitalize" placeholder="A Favorite Work (min: 1, max: 40)" name="workInfluence" value={authorData.workInfluence} required />
-        <textarea onChange={handleInputChange} type="textarea" className="textarea input  join-item w-full rounded-lg mb-1 capitalize" placeholder="Tell Us About Yourself (min: 1, max: 250)" name="aboutMe" value={authorData.aboutMe} required />
+
+      <div className="w-full sm:w-3/4 flex flex-col items-center border-2 border-base-300 bg-base-300 gap-1 rounded-lg">
+
+        <div className="flex flex-row w-full"><input onChange={handleInputChange} type="text" className="input join-item w-[90%] sm:w-[94%] rounded-tl-lg capitalize focus:outline-none" placeholder="Name (min: 1, max: 40)" name="authorName" value={authorData.authorName} required /><span className={`text-xs w-[10%] sm:w-[6%] flex justify-center items-center rounded-tr-lg font-bold ${authorData.authorName.length >= 41 ? 'bg-error' : 'bg-success/50'}`}>{authorData.authorName.length}</span></div>
+
+        <div className="flex flex-row w-full"><input onChange={handleInputChange} type="text" className="input join-item w-[90%] sm:w-[94%] capitalize focus:outline-none" placeholder="Influenced By (min: 1, max: 40)" name="authorInfluence" value={authorData.authorInfluence} required /><span className={`text-xs w-[10%] sm:w-[6%] flex justify-center items-center font-bold ${authorData.authorInfluence.length >= 41 ? 'bg-error' : 'bg-success/50'}`}>{authorData.authorInfluence.length}</span></div>
+
+        <div className="flex flex-row w-full"><input onChange={handleInputChange} type="text" className="input join-item w-[90%] sm:w-[94%] capitalize focus:outline-none" placeholder="A Favorite Work (min: 1, max: 40)" name="workInfluence" value={authorData.workInfluence} required /><span className={`text-xs w-[10%] sm:w-[6%] flex justify-center items-center font-bold ${authorData.workInfluence.length >= 41 ? 'bg-error' : 'bg-success/50'}`}>{authorData.workInfluence.length}</span></div>
+
+        <div className="flex flex-row w-full"><textarea onChange={handleInputChange} type="text" className="input join-item w-[89%] sm:w-[94%] capitalize focus:outline-none" placeholder="Tell Us About Yourself (min: 1, max: 250)" name="aboutMe" value={authorData.aboutMe} required /><span className={`text-xs w-[11%] sm:w-[6%] flex justify-center items-center font-bold ${authorData.aboutMe.length >= 251 ? 'bg-error' : 'bg-success/50'}`}>{authorData.aboutMe.length}</span></div>
+
         <input hidden readOnly type="text" className="input input-bordered join-item w-full" name="authUser" value={authorData.authUser} required />
-        <select onChange={handleInputChange} className="select  join-item w-full rounded-lg mb-1" name="publicProfile" value={authorData.publicProfile} >
+
+        <select onChange={handleInputChange} className="select  join-item w-full focus:outline-none mb-1" name="publicProfile" value={authorData.publicProfile} >
           {profileOptions}
         </select>
+
         <button className="w-1/3 btn btn-xs join-item rounded-lg mb-1 border-2 border-success" type="submit">Create Profile</button>
 
       </div>
