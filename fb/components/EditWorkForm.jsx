d02a379c0fc081ba01ed35a521696d657575ed6e
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-04-25 16:27:12 -0400

    Added character counter to all input fields on work and edit work forms

--- fb/components/EditWorkForm.jsx
+++ fb/components/EditWorkForm.jsx
@@ -7,7 +7,6 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 
 
-
 const EditWorkForm = ({ workToEdit }) => {
 
   const { workId, workTitle, workGenre, workSynopsis, workAuthorName, workAuthUser, workWritingStage, workCreatedBy } = workToEdit;
@@ -53,16 +52,25 @@ const EditWorkForm = ({ workToEdit }) => {
 
   return (
     <form onSubmit={handleSubmit} className="w-full flex flex-col justify-center items-center">
+
       <p className="capitalize m-4 font-serif text-lg">edit <span className="font-serif italic underline">{editWorkData.workTitle}</span></p>
-      <div className="w-full sm:w-3/4 flex flex-col items-center border-2 border-base-300 bg-base-300 rounded-lg">
-        <input onChange={handleInputChange} type="text" className="input join-item w-full rounded-lg capitalize mb-1" placeholder="Title (min: 1, max: 40)" name="workTitle" value={editWorkData.workTitle} required />
-        <input onChange={handleInputChange} type="text" className="input join-item w-full rounded-lg capitalize mb-1" placeholder="Genre (min: 1, max: 40)" name="workGenre" value={editWorkData.workGenre} required />
-        <textarea onChange={handleInputChange} type="textarea" className="textarea input join-item w-full rounded-lg capitalize mb-1" placeholder="Synopsis (min: 10, max: 250)" name="workSynopsis" value={editWorkData.workSynopsis} required />
-        <select onChange={handleInputChange} className="select join-item w-full rounded-lg mb-1" name="workWritingStage" value={editWorkData.workWritingStage} >
+
+      <div className="w-full sm:w-3/4 flex flex-col items-center border-2 border-base-300 bg-base-300 gap-1 rounded-lg">
+
+        <div className="flex flex-row w-full"><input onChange={handleInputChange} type="text" className="input join-item w-[90%] sm:w-[94%] rounded-tl-lg capitalize focus:outline-none" placeholder="Title (min: 1, max: 40)" name="workTitle" value={editWorkData.workTitle} required /><span className={`text-xs w-[10%] sm:w-[6%] flex justify-center items-center rounded-tr-lg font-bold ${editWorkData.workTitle.length >= 41 ? 'bg-error' : 'bg-success/50'}`}>{editWorkData.workTitle.length}</span></div>
+
+        <div className="flex flex-row w-full"><input onChange={handleInputChange} type="text" className="input join-item w-[90%] sm:w-[94%] capitalize focus:outline-none" placeholder="Genre (min: 1, max: 30)" name="workGenre" value={editWorkData.workGenre} required /><span className={`text-xs w-[10%] sm:w-[6%] flex justify-center items-center font-bold ${editWorkData.workGenre.length >= 31 ? 'bg-error' : 'bg-success/50'}`}>{editWorkData.workGenre.length}</span></div>
+
+        <div className="flex flex-row w-full"><textarea onChange={handleInputChange} type="text" className="input join-item w-[90%] sm:w-[94%] capitalize focus:outline-none" placeholder="Synopsis (min: 10, max: 250)" name="workSynopsis" value={editWorkData.workSynopsis} required /><span className={`text-xs w-[10%] sm:w-[6%] flex justify-center items-center font-bold ${editWorkData.workSynopsis.length >= 251 ? 'bg-error' : 'bg-success/50'}`}>{editWorkData.workSynopsis.length}</span></div>
+
+        <select onChange={handleInputChange} className="select join-item w-full mb-1" name="workWritingStage" value={editWorkData.workWritingStage} >
           {writingStateOptions}
         </select>
-        <div className="flex justify-center items-center w-full">
+
+        <div className="flex justify-center items-center w-full mb-1">
+
           <button className=" btn btn-xs join-item mr-2 border-2 border-success rounded-lg" type="submit">Apply Edits</button>
+
           <Link href={`/works/${workId}`} className="btn btn-xs rounded-lg">Work Page</Link>
         </div>
       </div>
--- fb/components/WorkForm.jsx
+++ fb/components/WorkForm.jsx
@@ -59,17 +59,25 @@ const WorkForm = ({ workAuthor }) => {
 
   return (
     <form onSubmit={handleSubmit} className="w-full flex flex-col justify-center items-center">
-      <div className="w-full sm:w-3/4 flex flex-col items-center border-2 border-base-300 bg-base-300 rounded-lg">
-        <input onChange={handleInputChange} type="text" className="input join-item w-full rounded-lg capitalize mb-1" placeholder="Title (min: 1, max: 40)" name="title" value={workData.title} required />
-        <input onChange={handleInputChange} type="text" className="input join-item w-full rounded-lg capitalize mb-1" placeholder="Genre (min: 1, max: 30)" name="genre" value={workData.genre} required />
-        <textarea onChange={handleInputChange} type="textarea" className="textarea input join-item w-full rounded-lg capitalize mb-1" placeholder="Synopsis (min: 10, max: 250)" name="synopsis" value={workData.synopsis} required />
+
+      <div className="w-full sm:w-3/4 flex flex-col items-center border-2 border-base-300 bg-base-300 gap-1 rounded-lg">
+
+        <div className="flex flex-row w-full"><input onChange={handleInputChange} type="text" className="input join-item w-[90%] sm:w-[94%] rounded-tl-lg capitalize focus:outline-none" placeholder="Title (min: 1, max: 40)" name="title" value={workData.title} required /><span className={`text-xs w-[10%] sm:w-[6%] flex justify-center items-center rounded-tr-lg font-bold ${workData.title.length >= 41 ? 'bg-error' : 'bg-success/50'}`}>{workData.title.length}</span></div>
+
+        <div className="flex flex-row w-full"><input onChange={handleInputChange} type="text" className="input join-item w-[90%] sm:w-[94%] capitalize focus:outline-none" placeholder="Genre (min: 1, max: 30)" name="genre" value={workData.genre} required /><span className={`text-xs w-[10%] sm:w-[6%] flex justify-center items-center font-bold ${workData.genre.length >= 31 ? 'bg-error' : 'bg-success/50'}`}>{workData.genre.length}</span></div>
+
+        <div className="flex flex-row w-full"><textarea onChange={handleInputChange} type="text" className="input join-item w-[90%] sm:w-[94%] capitalize focus:outline-none" placeholder="Synopsis (min: 10, max: 250)" name="synopsis" value={workData.synopsis} required /><span className={`text-xs w-[10%] sm:w-[6%] flex justify-center items-center font-bold ${workData.synopsis.length >= 251 ? 'bg-error' : 'bg-success/50'}`}>{workData.synopsis.length}</span></div>
+
         <input hidden readOnly type="text" className="input input-bordered join-item w-full" name="authUser" value={workData.authUser} required />
         <input hidden readOnly type="text" className="input input-bordered join-item w-full" name="authName" value={workData.authorName} required />
         <input hidden readOnly type="text" className="input input-bordered join-item w-full" name="createdBy" value={workData.createdBy} required />
-        <select onChange={handleInputChange} className="select join-item w-full rounded-lg mb-1" name="writingStage" value={workData.writingStage} >
+
+        <select onChange={handleInputChange} className="select join-item w-full mb-1 focus:outline-none" name="writingStage" value={workData.writingStage} >
           {writingStateOptions}
         </select>
+
         <button className="w-auto btn btn-xs border-2 border-success join-item rounded-lg mb-1" type="submit">Create Work</button>
+
       </div>
     </form>
   )
